a4a47db80f7abb6591ed9b9019f8e466073196e1
WallOliveira01 <contato.wallaceoliveira@outlook.com>
2019-09-17 16:40:03 -0300

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+use \Slim\Slim;
+use \Wall\Page;
+
+$app = new Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function(){
+
+	$page = new Page();
+
+	$page->setTpl("index");
+});
+
+$app->run();
+
+?>
